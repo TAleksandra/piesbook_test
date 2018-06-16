c13e9512f05530e0fdee0db5773089a204dff6bd
TAleksandra <tatara.ola@gmail.com>
2018-06-16 18:25:36 +0200

    dodanie zakładki z postami u admina i możliwości publikacji

--- forum/models.py
+++ forum/models.py
@@ -1,12 +1,20 @@
 from django.db import models
-
-# Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+from django.utils import timezone
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
