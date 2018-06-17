5ce7031ee4cc9c5fafde4d6d008f72b2f0365a29
WÓŁROBOCZY\Rafał <raphael.feldzensztajn@gmail.com>
2018-06-17 22:57:18 +0200

    Wrzucanie zdjęć poprzez link do strony xD

--- forum/models.py
+++ forum/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 from django.utils import timezone
-# from django.core.urlresolevers import revers
+from django.urls import reverse
+
+
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    image = models.FileField()
+    image = models.CharField(max_length=100000)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('forum:post_detail',kwargs={'pk':self.pk})
+
+
 
     def publish(self):
         self.published_date = timezone.now()
